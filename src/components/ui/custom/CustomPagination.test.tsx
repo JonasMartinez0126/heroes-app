2023771b7d35e72f2d58c50028819ba116806b5c
Jonas Martinez <jonasmartinez0126@gmail.com>
2025-12-22 16:16:22 -0300

    Se realizaron las pruebas respectivas

---
+++ src/components/ui/custom/CustomPagination.test.tsx
@@ -0,0 +1,72 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { describe, expect, test, vi } from "vitest";
+import { CustomPagination } from "./CustomPagination";
+import { MemoryRouter } from "react-router";
+import type { PropsWithChildren } from "react";
+
+vi.mock('../button', () => ({
+    Button: ({ children, ...props }: PropsWithChildren) => (
+        <button {...props}>{children}</button>
+    )
+}));
+
+
+const renderWithRouter = (
+    component: React.ReactElement,
+    initialEntries?: string[]
+) => {
+    return render(
+        <MemoryRouter
+            initialEntries={initialEntries}
+        >
+            {component}
+        </MemoryRouter>
+    )
+}
+
+describe('CustomPagination', () => {
+
+    test('should render component with default values', () => {
+        renderWithRouter(<CustomPagination totalPages={5} />);
+
+        expect(screen.getByText('Previous')).toBeDefined();
+        expect(screen.getByText('Next')).toBeDefined();
+    });
+
+    test('should disabled previous button when page is 1', () => {
+        renderWithRouter(<CustomPagination totalPages={5} />);
+        const previousButton = screen.getByText('Previous');
+
+        expect(previousButton.getAttributeNames()).toContain('disabled');
+    });
+
+    test('should disabled next button whe we are in the last page', () => {
+        renderWithRouter(<CustomPagination totalPages={5} />, ['/?page=5']);
+        const nextButton = screen.getByText('Next');
+
+        expect(nextButton.getAttributeNames()).toContain('disabled');
+    });
+
+    test('should disabled button 3 when we are in page 3', () => {
+        renderWithRouter(<CustomPagination totalPages={10} />, ['/?page=3']);
+        const button2 = screen.getByText('2');
+        const button3 = screen.getByText('3');
+
+        expect(button2.getAttribute('variant')).toBe('outline');
+        expect(button3.getAttribute('variant')).toBe('default');
+    });
+
+    test('should change page when click on number button', () => {
+        renderWithRouter(<CustomPagination totalPages={10} />, ['/?page=3']);
+
+        const button2 = screen.getByText('2');
+        const button3 = screen.getByText('3');
+
+        expect(button2.getAttribute('variant')).toBe('outline');
+        expect(button3.getAttribute('variant')).toBe('default');
+
+        fireEvent.click(button2);
+        expect(button2.getAttribute('variant')).toBe('default');
+        expect(button3.getAttribute('variant')).toBe('outline');
+    });
+});
---
+++ src/heroes/pages/home/HomePage.test.tsx
@@ -0,0 +1,68 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
+import { beforeEach, describe, expect, test, vi } from "vitest";
+import { HomePage } from "./HomePage";
+import { usePaginatedHero } from "@/heroes/hooks/usePaginatedHero";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { FavoriteHeroProvider } from "@/heroes/context/FavoriteHeroContext";
+
+vi.mock('@/heroes/hooks/usePaginatedHero');
+
+const mockUsePaginateHero = vi.mocked(usePaginatedHero);
+
+mockUsePaginateHero.mockReturnValue({
+    data: [],
+    isLoading: false,
+    isError: false,
+    isSuccess: true
+} as unknown as ReturnType<typeof mockUsePaginateHero>);
+
+const queryClient = new QueryClient();
+
+const renderHomePage = (initialEntries: string[] = ['/']) => {
+
+    return render(
+        <MemoryRouter initialEntries={initialEntries}>
+            <FavoriteHeroProvider>
+                <QueryClientProvider client={queryClient}>
+                    <HomePage />
+                </QueryClientProvider>
+            </FavoriteHeroProvider>
+        </MemoryRouter>
+    )
+}
+
+describe('HomePage', () => {
+
+    beforeEach(() => {
+        vi.clearAllMocks();
+    });
+
+    test('should render with HomePage with default value', () => {
+        const { container } = renderHomePage();
+
+        expect(container).toMatchSnapshot();
+    });
+
+    test('should call usePaginatedHero with default values', () => {
+        renderHomePage();
+
+        expect(mockUsePaginateHero).toHaveBeenCalledWith(1, 6, 'all');
+    });
+
+    test('should call usePaginatedHero with custom query params', () => {
+        renderHomePage(['/?page=2&limit=10&category=villains']);
+
+        expect(mockUsePaginateHero).toHaveBeenCalledWith(2, 10, 'villains');
+    });
+
+    test('should call usePaginatedHero with default page and same limit on tab clicked', () => {
+        renderHomePage(['/?tab=favorites&page=2&limit=10']);
+
+        const [, , , villainsTab] = screen.getAllByRole('tab');
+
+        fireEvent.click(villainsTab);
+
+        expect(mockUsePaginateHero).toHaveBeenCalledWith(1, 10, 'villain');
+    });
+});
